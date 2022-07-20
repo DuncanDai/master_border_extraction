4c8e91d1d42f31d7e6dfd276e775804b10897bcd
DaiDuncan <942856733@qq.com>
2022-07-20 10:33:26 +0200

    Milestone - env prepared

--- pre_processing.py
+++ pre_processing.py
@@ -1,35 +1,13 @@
-#!/usr/bin/python3 
-import os 
-from shutil import copy
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   pre_processing.py
+@Time    :   2022/07/20 10:30:36
+@Author  :   Dinggen DAI
+@Version :   1.0
+@Contact :   daidinggen@hotmail.com
+@License :   Copyright (c) 2022-2022 Dinggen DAI. All rights reserved.
+@Desc    :   []
+'''
 
-old_path = r'/media/dinggen/hdd1/Image'
-new_path = r'/home/dinggen/Documents/Proj_KITMA/kitma/images'
-# new_path = r'smb://192.168.1.102/d/My_Data/me_Docs/Masterarbeit/images' # different platform: error
-
-# Get 
-sub_dirs = os.listdir(old_path)
-dirs_num = len(sub_dirs)
-
-for sub_dir in sub_dirs:
-    print(f'The subdirectory is {sub_dir}.')
-    src_path = os.path.join(old_path, sub_dir)
-    dst_path = os.path.join(new_path, sub_dir)
-
-    if not os.path.isdir(dst_path):
-        os.makedirs(dst_path)
-    
-    files = [f for f in os.listdir(src_path)]
-    files_num = len(files)
-    # choose 20 images in each folder
-    choose_files_index = list(range(0, files_num, files_num//19))
-
-    for index in choose_files_index:
-        src_file = os.path.join(src_path, files[int(index)])
-        copy(src_file, dst_path)
-
-    
-    
-
-
-
-
+# here put the import lib
---
+++ prework_sample_images.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3 
+import os 
+from shutil import copy
+
+old_path = r'/media/dinggen/hdd1/Image'
+new_path = r'/home/dinggen/Documents/Proj_KITMA/kitma/images'
+# new_path = r'smb://192.168.1.102/d/My_Data/me_Docs/Masterarbeit/images' # different platform: error
+
+# Get the directories 
+sub_dirs = os.listdir(old_path)
+dirs_num = len(sub_dirs)
+
+for sub_dir in sub_dirs:
+    print(f'The subdirectory is {sub_dir}.')
+    src_path = os.path.join(old_path, sub_dir)
+    dst_path = os.path.join(new_path, sub_dir)
+
+    if not os.path.isdir(dst_path):
+        os.makedirs(dst_path)
+    
+    files = [f for f in os.listdir(src_path)]   # output of os.listdir(src_path) is a list of 'XXX.png'
+    files_num = len(files)
+    # choose 20 images in each directory/folder
+    choose_files_index = list(range(0, files_num, files_num//19))
+
+    for index in choose_files_index:
+        src_file = os.path.join(src_path, files[int(index)])
+        copy(src_file, dst_path)
